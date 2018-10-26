7317f364c79add97cd97da1cd06f6ae099f7f3df
naga <naga@area-b.com>
2018-10-26 23:23:15 +0900

    タスク一覧追加

--- admin-view/memo.md
+++ admin-view/memo.md
@@ -6,5 +6,12 @@
 $ npm install -g create-react-app
 $ create-react-app admin-view
 $ cd admin-view/
+$ yarn add react-admin prop-types
 $ yarn start
+```
+
+## ローカルのJSONを読み込んで動かす
+
+```
+$ yarn add ra-data-fakerest
 ```
--- admin-view/src/App.js
+++ admin-view/src/App.js
@@ -1,28 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Admin, Resource } from 'react-admin'
+import fakeDataProvider from 'ra-data-fakerest'
+import { TaskList } from './tasks/TaskList'
+import tasks from './mock-data/tasks'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+console.log(tasks)
+const dataProvider = fakeDataProvider(tasks)
 
-export default App;
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="tasks" list={TaskList} />
+  </Admin>
+)
+
+export default App
