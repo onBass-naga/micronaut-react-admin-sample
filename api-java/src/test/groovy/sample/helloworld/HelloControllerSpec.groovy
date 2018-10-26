7e0e71fc46ace4f28bae29b5cbd3954cb7bb98c7
naga <naga@area-b.com>
2018-10-26 21:30:42 +0900

    テスト追加。 groovyを使えるように設定変更。

---
+++ api-java/src/test/groovy/sample/helloworld/HelloControllerSpec.groovy
@@ -0,0 +1,24 @@
+package sample.helloworld
+
+import io.micronaut.context.ApplicationContext
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.client.HttpClient
+import io.micronaut.runtime.server.EmbeddedServer
+import spock.lang.*
+
+class HelloControllerSpec extends Specification {
+
+    @Shared
+    @AutoCleanup
+    EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer)
+
+    @Shared
+    @AutoCleanup
+    HttpClient client = HttpClient.create(embeddedServer.URL)
+
+    void "test hello world response"() {
+        expect:
+        client.toBlocking()
+                .retrieve(HttpRequest.GET('/hello')) == "Hello World"
+    }
+}
