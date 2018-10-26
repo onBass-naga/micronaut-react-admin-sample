5a05c8293efaa2292bbb48fc185ec07d423d4e78
naga <naga@area-b.com>
2018-10-26 21:43:23 +0900

    Client追加

---
+++ api-java/src/main/java/sample/helloworld/HelloClient.java
@@ -0,0 +1,11 @@
+package sample.helloworld;
+
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.client.annotation.Client;
+import io.reactivex.Single;
+
+@Client("/hello")
+public interface HelloClient {
+    @Get
+    Single<String> hello();
+}
